9f1a91fa5e0a4bfcdd935beb423ee901de4d8492
brennokayan <brennokayan@hotmail.com>
2022-08-15 15:52:16 -0400

    ajuste para o deploy do back-end

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
-export const port  = 3333
+export const port  = 3000
 const app = express();
 
 app.use(cors());
